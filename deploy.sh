3b2e55c33342f78dde899345c3e655daaae9d87c
Andrew A. Chuev <andrew.chuev@gmail.com>
2025-03-05 23:03:15 +0200

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 git pull origin main
-composer install
-npm install
+
+# Устанавливаем зависимости
+echo "Installing dependencies..."
+composer install --no-dev --optimize-autoloader || die "Composer install failed."
+
+# Запускаем миграции
+echo "Running migrations..."
+php artisan migrate --force || die "Migrations failed."
+
+# Кешируем конфигурацию, маршруты и представления
+php artisan config:cache || die "Config caching failed."
+php artisan route:cache || die "Route caching failed."
+php artisan view:cache || die "View caching failed."
+
 php artisan migrate --force
+
+npm install
+
+
 echo "************* Done!!!!!!!!!!!!!!!"
